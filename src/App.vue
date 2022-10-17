c585d697f87c63c486e709494afacbf69f3f6947
jcgabourel <40446669+jcgabourel@users.noreply.github.com>
2022-10-17 15:56:38 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import CUSideBar from './components/Layout/sidebar.vue' ;
+import CUwrapper from './components/Layout/wrapper.vue' ;
+
+
+</script>
+
+<template>
+<div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
+  <CUSideBar />
+</div>
+<div class="wrapper d-flex flex-column min-vh-100 bg-light">
+  <CUwrapper />
+</div>
+
+</template>
+
+<style lang="scss">
+// Import Main styles for this application
+@import 'styles/style';
+
+
+</style>
---
+++ src/components/Layout/wrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  
+    <header class="header header-sticky mb-4">
+        <CUheader />
+    </header>
+
+    <div class="body flex-grow-1 px-3"></div>
+    <footer class="footer"></footer>
+  
+</template>
+
+<script setup>
+import CUheader from "./header.vue" ;
+
+</script>
